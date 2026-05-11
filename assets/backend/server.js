e1d73bdbe0f5250bd39ea0b891faed598c5b159b
Coutinho5491 <ctnhgabriel@gmail.com>
2026-05-11 10:31:27 -0300

    implementação do banco de dados para armazenamento dos dados de login do usuário

--- assets/backend/server.js
+++ assets/backend/server.js
@@ -1,3 +1,6 @@
+/*--------------IMPORTS--------------------*/
+
+const { Pool } = require('pg');
 const express = require('express');   /*criar um sistema que escuta requisições*/
 const cors = require('cors');         
 
@@ -6,71 +9,117 @@ const jwt = require('jsonwebtoken');
 
 const app = express();                  /*criar um sistema que escuta requisições*/  
 
-app.use(cors());            /*Front (HTML) converse com o backend*/
+app.use(cors());            /*Front (HTML) conversa com o backend*/
 app.use(express.json());    /*Permite receber dados em formato JSON*/
+
+/*-------------CONEXÃO POSTGRESQL---------------------*/
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ecommerce',
+  password: '947t#eE@65',
+  port: 5432,
+});
+
+pool.connect()
+  .then(() => console.log('Conectado ao PostgreSQL'))
+  .catch(err => console.error('Erro ao conectar', err));
 
 /*----------------------------------*/
 
 app.post('/auth/login', async (req, res) => {           /*Sistema de login, 'cria uma rota'*/
-  const { email, password } = req.body;                  /*pega o que o usuário digitou no front*/
 
-  const user = users.find(u => u.email === email);                        /*Verifica se o e-mail existe*/
+  try {
 
-  if (!user) {
-    return res.status(400).json({ error: 'Usuário não encontrado' });
+    const { email, password } = req.body;           /*pega o que o usuário digitou no front*/
+
+    const result = await pool.query(
+      'SELECT * FROM users WHERE email = $1',            
+      [email]
+    );
+
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(400).json({
+        error: 'Usuário não encontrado'
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,                                                  /*senha é criptografada*/
+      user.password
+    );
+
+    if (!validPassword) {
+      return res.status(400).json({
+        error: 'Senha inválida'
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id },                            /* usuário está autenticado */
+      'segredo'
+    );
+
+    res.json({ token });                         /* manda o token de volta */
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: 'Erro no servidor'
+    });
   }
-
-  const validPassword = await bcrypt.compare(password, user.password);            /*senha é criptografada*/
-
-  if (!validPassword) {
-    return res.status(400).json({ error: 'Senha inválida' });
-  }
-
-  const token = jwt.sign({ id: user.id }, 'segredo');            /* usuário está autenticado */
-
-  res.json({ token });                           /* manda o token de volta */
 });
 
 /*----------------------------------*/
 
 /*---------------------------------SCRIPT PARA CADASTRO----------------------------*/
 
-app.post('/auth/register', async (req, res) => {        /*Quando alguém fizer um POST em /auth/register, execute*/
 
-  const { name, email, password, cpf } = req.body;          /*pega o que o usuário digitou*/
 
-  // Verificar se o email já existe
+app.post('/auth/register', async (req, res) => {
 
-  const userExists = users.find(u => u.email === email);    /*Procura no array “banco”*/
+  const { name, email, password, cpf } = req.body;
 
-  if (userExists) {                                             /*Se já existir, para e retorna erro*/
-    return res.status(400).json({ error: 'Usuário já existe' });   
+  try {
+
+    const userExists = await pool.query(
+      'SELECT * FROM users WHERE email = $1 OR cpf = $2',
+      [email, cpf]
+    );
+
+    if (userExists.rows.length > 0) {
+      return res.status(400).json({
+        error: 'E-mail ou CPF já cadastrado'
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await pool.query(
+      'INSERT INTO users (name, email, cpf, password) VALUES ($1, $2, $3, $4)',
+      [name, email, cpf, hashedPassword]
+    );
+
+    res.status(201).json({
+      message: 'Usuário criado com sucesso'
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: 'Erro no servidor'
+    });
   }
-
-  // Verifica se o CPF ja existe
-const cpfExists = users.find(u => u.cpf === cpf);            // procura se cpf já existe cadastrado
-
-  if (cpfExists) {                                              // Se existir é bloqueado
-    return res.status(400).json({ error: 'CPF já cadastrado' });
-  }  
-
-  /*----------------------------------*/
-
-  const hashedPassword = await bcrypt.hash(password, 10);         /*Criptografa a senha*/
-
-  const newUser = {                                   /*criando o usuario*/
-    id: users.length + 1,
-    name,
-    email,
-    cpf,
-    password: hashedPassword
-  };
-
-  users.push(newUser);          /*Adiciona no array "BANCO")*/
-
-  res.status(201).json({ message: 'Usuário criado com sucesso' });      /*resposta para o front*/
-
 });
+
+
 
 /*---------------------------------FIM SCRIPT PARA CADASTRO----------------------------*/
 
@@ -82,12 +131,6 @@ app.listen(3000, () => {
 
 /*----------------------------------*/
 
-const users = [
-  {
-    id: 1,
-    email: "teste@email.com",
-    password: "$2b$10$Q.1xjrQbi1kTqIfKyLVALeBQHPgEvX0afTHuk4T1wyrnKYYX7S/1u"
-  }
-];
+
 
 /*----------------------------------*/
